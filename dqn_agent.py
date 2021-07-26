ef569e376c6f57fd192d28941165adedd780d0d1
Maxence Faldor <maxencefaldor@gmail.com>
2021-07-26 09:50:25 -0700

    Fix loss when using PER
    Weight importance sampling was wrong.

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import io
+import base64
+import shutil
 import math
 import random
 from copy import deepcopy
-from itertools import count, product
+from itertools import count
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython import display
+from IPython.display import HTML
 
 from replay_memory import replay_buffer
 from replay_memory import prioritized_replay_buffer
 import gym
 from gym import logger
 logger.set_level(gym.logger.DISABLED)
+from gym.wrappers import AtariPreprocessing
+from gym.wrappers import FrameStack
+from gym.wrappers import Monitor
 
 import torch
 import torch.nn as nn
@@ -25,9 +33,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(4, 16)
+        self.fc1 = nn.Linear(2, 16)
         self.fc2 = nn.Linear(16, 16)
-        self.fc3 = nn.Linear(16, 2)
+        self.fc3 = nn.Linear(16, 3)
     
     def forward(self, x):
         x = x.to(device)
@@ -99,13 +107,13 @@ class DQNAgent:
     def epsilon_greedy_action(self, state):
         if random.random() > self.epsilon:
             with torch.no_grad():
-                return torch.argmax(self.dqn(torch.Tensor(state))).item()
+                return torch.argmax(self.dqn(torch.Tensor(state).to(device).unsqueeze(0))).item()
         else:
             return torch.tensor(random.randrange(self.n_actions), device=device, dtype=torch.long).item()
     
     def greedy_action(self, state):
         with torch.no_grad():
-            return torch.argmax(self.dqn(torch.Tensor(state))).item()
+            return torch.argmax(self.dqn(torch.Tensor(state).to(device).unsqueeze(0))).item()
     
     def vanilla_update(self, next_state_batch):
         return self.dqn(next_state_batch).max(1)[0].unsqueeze(1).detach()
@@ -126,21 +134,23 @@ class DQNAgent:
             batch = self.replay_buffer.sample(self.batch_size)
         batch = [*zip(*batch)]
         
-        state_batch = torch.stack(batch[0])
-        action_batch = torch.stack(batch[1])
-        reward_batch = torch.stack(batch[2])
-        next_state_batch = torch.stack(batch[3])
-        done_batch = torch.stack(batch[4])
+        state_batch = torch.stack(batch[0]).to(device)
+        action_batch = torch.stack(batch[1]).to(device)
+        reward_batch = torch.stack(batch[2]).to(device)
+        next_state_batch = torch.stack(batch[3]).to(device)
+        done_batch = torch.stack(batch[4]).to(device)
         
         state_action_values = self.dqn(state_batch).gather(1, action_batch)
         next_state_action_values = self.update_rule(next_state_batch)
         expected_state_action_values = reward_batch + self.gamma * next_state_action_values * (1 - done_batch)
         
         if self.per:
-            errors = (state_action_values - expected_state_action_values).data.numpy()
+            errors = state_action_values - expected_state_action_values
             for i, index in enumerate(indices):
-                self.replay_buffer.update(index, errors[i])
-            loss = self.criterion(state_action_values, torch.tensor(is_weights, dtype=torch.float32)*expected_state_action_values)
+                self.replay_buffer.update(index, errors[i][0].item())
+            loss = F.mse_loss(state_action_values, expected_state_action_values, reduction='none')
+            loss *= torch.tensor(is_weights.reshape(-1, 1), dtype=torch.float32).to(device)
+            loss = loss.mean()
         else:
             loss = self.criterion(state_action_values, expected_state_action_values)
         
@@ -180,142 +190,93 @@ class DQNAgent:
                     self.learn()
                 
                 if done:
+                    if return_list and episode_return >= max(return_list):
+                        self.save("model.pt")
                     return_list.append(episode_return)
                     print("Episode {:4d} : {:4d} steps | epsilon = {:4.2f} | return = {:.1f}".format(i_episode, t+1, self.epsilon, episode_return))
                     break
         return return_list
     
-    def test(self, env, step_max):
+    def test(self, env, step_max=np.inf, render_video=True):
+        if render_video:
+            env = Monitor(env, "./tmp", force=True)
         episode_return = 0
         state = env.reset()
         for t in count():
-            env.render()
+            if not render_video:
+                env.render()
             action = self.greedy_action(state)
             next_state, reward, done, _ = env.step(action)
+
             episode_return += reward
-            
             state = next_state
 
             if done or t+1 >= step_max:
                 env.close()
-                return episode_return
+                if render_video:
+                    video = io.open("./tmp/openaigym.video.{}.video000000.mp4".format(env.file_infix), 'r+b').read()
+                    encoded = base64.b64encode(video)
+                    return HTML(data='''
+                        <video width="360" height="auto" alt="test" controls><source src="data:video/mp4;base64,{0}" type="video/mp4" /></video>'''
+                    .format(encoded.decode('ascii')))
+                    shutil.rmtree("./tmp")
     
-    def save(self, name):
-        torch.save(self.dqn.state_dict(), "trained_dqn/{}.pt".format(name))
+    def save(self, path):
+        torch.save(self.dqn.state_dict(), path)
     
-    def load(self, name):
-        self.dqn.load_state_dict(torch.load("trained_dqn/{}.pt".format(name)))
+    def load(self, path):
+        self.dqn.load_state_dict(torch.load(path))
+
 
 
 if __name__ == "__main__":
-    class CartPoleSwingUp(gym.Wrapper):
-        def __init__(self, env, **kwargs):
-            super(CartPoleSwingUp, self).__init__(env, **kwargs)
-            self.theta_dot_threshold = 4*np.pi
+    import time
+    from gym.wrappers import AtariPreprocessing
+    from gym.wrappers import FrameStack
     
-        def reset(self):
-            self.env.env.state = [0, 0, np.pi, 0] + super().reset()
-            self.env.env.steps_beyond_done = None
-            return np.array(self.env.env.state)
+    class ActionWrapper(AtariPreprocessing):
+        def __init__(self, env, **kwargs):
+            super(ActionWrapper, self).__init__(env, **kwargs)
     
         def step(self, action):
-            state, reward, done, _ = super().step(action)
-            x, x_dot, theta, theta_dot = state
-            
-            done = x < -self.x_threshold \
-                   or x > self.x_threshold \
-                   or theta_dot < -self.theta_dot_threshold \
-                   or theta_dot > self.theta_dot_threshold
-            
-            if done:
-                # game over
-                reward = -10.
-                if self.steps_beyond_done is None:
-                    self.steps_beyond_done = 0
-                else:
-                    logger.warn("You are calling 'step()' even though this environment has already returned done = True. You should always call 'reset()' once you receive 'done = True' -- any further steps are undefined behavior.")
-                    self.steps_beyond_done += 1
-            else:
-                if -self.theta_threshold_radians < theta and theta < self.theta_threshold_radians:
-                    # pole upright
-                    reward = 1.
-                else:
-                    # pole swinging
-                    reward = 0.
+            return super(ActionWrapper, self).step(4 + action)
     
-            return np.array(self.state), reward, done, {}
+    env = FrameStack(ActionWrapper(gym.make('PongNoFrameskip-v4'),
+                             noop_max=0,
+                             frame_skip=4,
+                             terminal_on_life_loss=True,
+                             grayscale_obs=True,
+                             scale_obs=True), 4)
     
-    env = CartPoleSwingUp(gym.make('CartPole-v1'))
+    class AtariCNN(nn.Module):
+        def __init__(self, in_channels=4, n_actions=2):
+            super(AtariCNN, self).__init__()
+            self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+            self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+            self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+            self.fc1 = nn.Linear(7 * 7 * 64, 512)
+            self.fc2 = nn.Linear(512, n_actions)
+        
+        def forward(self, x):
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = F.relu(self.conv3(x))
+            x = F.relu(self.fc1(x.view(x.size(0), -1)))
+            return self.fc2(x)
     
-    dqn_1 = DQN()
-    dqn_2 = deepcopy(dqn_1)
-    dqn_3 = deepcopy(dqn_1)
-    dqn_4 = deepcopy(dqn_1)
+    dqn = AtariCNN()
     
-    agent_1 = DQNAgent(2,
-                     dqn_1,
+    agent = DQNAgent(2,
+                     dqn,
                      lr=0.001,
                      gamma=0.99,
                      batch_size=32,
                      n_gradient_steps=1,
                      epsilon_min=0.01,
-                     epsilon_decay=2000,
+                     epsilon_decay=6000,
                      per=True,
-                     buffer_size=1e6,
-                     beta=100,
-                     option=0)
-    agent_2 = DQNAgent(2,
-                     dqn_2,
-                     lr=0.001,
-                     gamma=0.99,
-                     batch_size=32,
-                     n_gradient_steps=1,
-                     epsilon_min=0.01,
-                     epsilon_decay=2000,
-                     per=False,
-                     buffer_size=1e6,
-                     beta=100,
-                     option=0)
-    agent_3 = DQNAgent(2,
-                     dqn_3,
-                     lr=0.001,
-                     gamma=0.99,
-                     batch_size=32,
-                     n_gradient_steps=1,
-                     epsilon_min=0.01,
-                     epsilon_decay=2000,
-                     per=True,
-                     buffer_size=1e6,
+                     buffer_size=1e5,
                      beta=100,
                      option=2)
-    agent_4 = DQNAgent(2,
-                     dqn_4,
-                     lr=0.001,
-                     gamma=0.99,
-                     batch_size=32,
-                     n_gradient_steps=1,
-                     epsilon_min=0.01,
-                     epsilon_decay=2000,
-                     per=False,
-                     buffer_size=1e6,
-                     beta=100,
-                     option=2)
-    return_list_1 = agent_1.train(env, 200)
-    return_list_2 = agent_2.train(env, 200)
-    return_list_3 = agent_3.train(env, 200)
-    return_list_4 = agent_4.train(env, 200)
+    agent.train(env, 25)
     
-    plt.plot(return_list_1, label="Agent 1")
-    plt.plot(return_list_2, label="Agent 2")
-    plt.plot(return_list_3, label="Agent 3")
-    plt.plot(return_list_4, label="Agent 4")
-    plt.xlabel("episode")
-    plt.ylabel("return")
-    plt.legend()
-    plt.savefig('plot_2.png', dpi=1200)
-    plt.show()
-    
-    agent_1.test(env, 1000)
-    agent_2.test(env, 1000)
-    agent_3.test(env, 1000)
-    agent_4.test(env, 1000)
