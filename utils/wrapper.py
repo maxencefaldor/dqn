fb3116f1025ce0ccc11a56b1cd1fd3784dea7e99
Maxence Faldor <maxencefaldor@gmail.com>
2021-07-27 06:44:59 -0700

    Generalize Pong Wrapper

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -6,21 +6,23 @@ from gym.wrappers import AtariPreprocessing
 from gym.wrappers import FrameStack
 
 
-def make_atari(env_id):
+class AtariWrapper(AtariPreprocessing):
+    def __init__(self, env, action_map, **kwargs):
+        super(AtariWrapper, self).__init__(env, **kwargs)
+        self.action_map = action_map
+    
+    def step(self, action):
+        return super(AtariWrapper, self).step(self.action_map[action])
+
+def make_atari(env_id, action_map):
     env = gym.make(env_id)
     assert "NoFrameskip" in env.spec.id
-    env = AtariPreprocessing(env,
-                             noop_max=0,
-                             frame_skip=4,
-                             terminal_on_life_loss=True,
-                             grayscale_obs=True,
-                             scale_obs=True)
+    env = AtariWrapper(env,
+                       action_map=action_map,
+                       noop_max=0,
+                       frame_skip=4,
+                       terminal_on_life_loss=True,
+                       grayscale_obs=True,
+                       scale_obs=True)
     env = FrameStack(env, 4)
     return env
-
-class PongWrapper(AtariPreprocessing):
-    def __init__(self, env, **kwargs):
-        super(PongWrapper, self).__init__(env, **kwargs)
-
-    def step(self, action):
-        return super(PongWrapper, self).step(4 + action)
