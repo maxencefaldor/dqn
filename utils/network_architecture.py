d535dd159b4a525a85a23d63a6e838a102e62783
Maxence Faldor <maxencefaldor@gmail.com>
2021-07-29 12:01:03 -0700

    Add dueling network architectures

--- utils/network_architecture.py
+++ utils/network_architecture.py
@@ -33,3 +33,48 @@ class AtariNetwork(nn.Module):
         x = F.relu(self.conv3(x))
         x = F.relu(self.fc1(x.view(x.size(0), -1)))
         return self.fc2(x)
+
+class DuelingCartpoleNetwork(nn.Module):
+    def __init__(self):
+        super(DuelingCartpoleNetwork, self).__init__()
+        self.fc1 = nn.Linear(4, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.value_stream = nn.Sequential(nn.Linear(16, 1))
+        self.advantage_stream = nn.Sequential(nn.Linear(16, 2))
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        value = self.value_stream(x)
+        advantages = self.advantage_stream(x)
+        return value + (advantages - advantages.mean())
+
+class DuelingAtariNetwork(nn.Module):
+    def __init__(self, n_actions):
+        super(DuelingAtariNetwork, self).__init__()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(4, 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU())
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(7 * 7 * 64, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1))
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(7 * 7 * 64, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        value = self.value_stream(x)
+        advantages = self.advantage_stream(x)
+        return value + (advantages - advantages.mean())
