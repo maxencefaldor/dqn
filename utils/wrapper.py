a87c9814eb3ab673ec0d2257649220695e2c126f
Maxence Faldor <maxencefaldor@gmail.com>
2021-07-29 09:49:36 -0700

    Add comments to functions

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -49,6 +49,20 @@ class CartPoleSwingUp(gym.Wrapper):
         return np.array(self.state), reward, done, {}
 
 def make_cartpole_swing_up(env_id, max_episode_steps):
+    """Returns a CartPole Swing Up environment
+    
+    The environment is similar to the classic control CartPole environment,
+    though more complex. The pole starts hanging down and the cart must first
+    swing the pole to an upright position before balancing it as in normal
+    CartPole.
+    
+    Args:
+        env_id: str, environment id.
+        max_episode_steps: int, maximum number of steps by episode.
+    
+    Returns:
+        Gym environment.
+    """
     env = gym.make(env_id)
     assert "CartPole" in env.spec.id
     env = CartPoleSwingUp(env)
@@ -64,6 +78,33 @@ class AtariWrapper(AtariPreprocessing):
         return super(AtariWrapper, self).step(self.action_map[action])
 
 def make_atari(env_id, action_map):
+    """Returns an Atari 2600 Gym environment.
+    
+    The environment follows the guidelines in Machado et al. (2018),
+    "Revisiting the Arcade Learning Environment: Evaluation Protocols and Open
+    Problems for General Agents".
+    Specifically:
+    * NoopReset: obtain initial state by taking random number of no-ops on
+        reset.
+    * Frame skipping: 4 by default
+    * Max-pooling: most recent two observations
+    * Termination signal when a life is lost: turned off by default. Not
+        recommended by Machado et al. (2018).
+    * Resize to a square image: 84x84 by default
+    * Grayscale observation: optional
+    * Scale observation: optional
+    * Frame Stacking
+    
+    Args:
+        env_id: str, environment id.
+        action_map: dict, a dictionnary to map integer from [0, ..., n] to a
+            different list of integer [a_1, ... a_n]. For example, in the Pong
+            environment, the only two useful actions are 4 and 5. A dict can
+            be used to map these action back to 0 and 1.
+    
+    Returns:
+        Gym environment.
+    """
     env = gym.make(env_id)
     assert "NoFrameskip" in env.spec.id
     env = AtariWrapper(env,
