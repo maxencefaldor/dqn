cba825152e4a0f9a380642f048866cf305df3a5d
Maxence Faldor <maxencefaldor@gmail.com>
2021-07-31 05:58:49 -0700

    Add C51 network architectures
    for:
    - CartPole
    - Atari 2600

--- utils/network_architecture.py
+++ utils/network_architecture.py
@@ -6,6 +6,7 @@ Network architectures suited for CartPole and Atari 2600 environments. It
 includes normal and dueling networks.
 """
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -20,6 +21,7 @@ class CartpoleNetwork(nn.Module):
             n_neurons: int, number of neurons of the hidden layers.
         """
         super(CartpoleNetwork, self).__init__()
+        self.n_neurons = n_neurons
         self.fc1 = nn.Linear(4, n_neurons)
         self.fc2 = nn.Linear(n_neurons, n_neurons)
         self.fc3 = nn.Linear(n_neurons, 2)
@@ -40,6 +42,7 @@ class AtariNetwork(nn.Module):
             n_actions: int, number of actions possible for the agent.
         """
         super(AtariNetwork, self).__init__()
+        self.n_actions = n_actions
         self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
@@ -53,6 +56,7 @@ class AtariNetwork(nn.Module):
         x = F.relu(self.fc1(x.view(x.size(0), -1)))
         return self.fc2(x)
 
+
 class DuelingCartpoleNetwork(nn.Module):
     """Dueling network architecture suited for CartPole environment."""
     
@@ -63,6 +67,7 @@ class DuelingCartpoleNetwork(nn.Module):
             n_neurons: int, number of neurons of the hidden layers.
         """
         super(DuelingCartpoleNetwork, self).__init__()
+        self.n_neurons = n_neurons
         self.fc1 = nn.Linear(4, n_neurons)
         self.fc2 = nn.Linear(n_neurons, n_neurons)
         self.value_stream = nn.Sequential(nn.Linear(n_neurons, 1))
@@ -85,7 +90,7 @@ class DuelingAtariNetwork(nn.Module):
             n_actions: int, number of actions possible for the agent.
         """
         super(DuelingAtariNetwork, self).__init__()
-        
+        self.n_actions = n_actions
         self.conv = nn.Sequential(
             nn.Conv2d(4, 32, kernel_size=8, stride=4),
             nn.ReLU(),
@@ -93,12 +98,10 @@ class DuelingAtariNetwork(nn.Module):
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU())
-
         self.value_stream = nn.Sequential(
             nn.Linear(7 * 7 * 64, 512),
             nn.ReLU(),
             nn.Linear(512, 1))
-
         self.advantage_stream = nn.Sequential(
             nn.Linear(7 * 7 * 64, 512),
             nn.ReLU(),
@@ -110,3 +113,61 @@ class DuelingAtariNetwork(nn.Module):
         value = self.value_stream(x)
         advantages = self.advantage_stream(x)
         return value + (advantages - advantages.mean())
+
+
+class C51CartpoleNetwork(nn.Module):
+    """C51 Network architecture suited for CartPole environment."""
+    
+    def __init__(self, n_neurons=16, n_atoms=51, v_min=-10, v_max=10):
+        """Creates the layers.
+        
+        Args:
+            n_neurons: int, number of neurons of the hidden layers.
+        """
+        super(C51CartpoleNetwork, self).__init__()
+        self.neurons = n_neurons
+        self.n_atoms = n_atoms
+        self.support = torch.linspace(v_min, v_max, n_atoms)
+        self.fc1 = nn.Linear(4, n_neurons)
+        self.fc2 = nn.Linear(n_neurons, n_neurons)
+        self.fc3 = nn.Linear(n_neurons, 2)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        logits = x.view(-1, 2, self.n_atoms)
+        probabilities = F.softmax(logits, dim=2)
+        q_values = torch.sum(self.support*probabilities, dim=2)
+        return logits, probabilities, q_values
+
+class C51AtariNetwork(nn.Module):
+    """C51 Network architecture suited for Atari 2600 environment."""
+    
+    def __init__(self, n_actions, n_atoms=51, v_min=-10, v_max=10):
+        """Creates the layers.
+        
+        Args:
+            n_actions: int, number of actions possible for the agent.
+            n_atoms: int, number of atoms for discretization of the support.
+        """
+        super(C51AtariNetwork, self).__init__()
+        self.n_actions = n_actions
+        self.n_atoms = n_atoms
+        self.support = torch.linspace(v_min, v_max, n_atoms)
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(7 * 7 * 64, 512)
+        self.fc2 = nn.Linear(512, n_actions*n_atoms)
+    
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc1(x.view(x.size(0), -1)))
+        x = self.fc2(x)
+        logits = x.view(-1, self.n_actions, self.n_atoms)
+        probabilities = F.softmax(logits, dim=2)
+        q_values = torch.sum(self.support*probabilities, dim=2)
+        return logits, probabilities, q_values
