889a80d915eb772a97a6fa530db6c35107c9dcf7
Maxence Faldor <maxencefaldor@gmail.com>
2021-08-04 10:23:18 -0700

    Implement NoisyLinear layer

--- utils/network_architectures.py
+++ utils/network_architectures.py
@@ -6,9 +6,62 @@ Network architectures suited for CartPole and Atari 2600 environments. It
 includes normal, dueling and distributional networks.
 """
 
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
+class NoisyLinear(nn.Module):
+    def __init__(self, in_features, out_features, std_init=0.5):
+        super(NoisyLinear, self).__init__()
+        
+        self.in_features  = in_features
+        self.out_features = out_features
+        self.std_init     = std_init
+        
+        self.weight_mu    = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        self.weight_sigma = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        self.register_buffer('weight_epsilon', torch.FloatTensor(out_features, in_features))
+        
+        self.bias_mu    = nn.Parameter(torch.FloatTensor(out_features))
+        self.bias_sigma = nn.Parameter(torch.FloatTensor(out_features))
+        self.register_buffer('bias_epsilon', torch.FloatTensor(out_features))
+        
+        self.reset_parameters()
+        self.reset_noise()
+    
+    def forward(self, x):
+        if self.training: 
+            weight = self.weight_mu + self.weight_sigma.mul(self.weight_epsilon)
+            bias   = self.bias_mu   + self.bias_sigma.mul(self.bias_epsilon)
+        else:
+            weight = self.weight_mu
+            bias   = self.bias_mu
+        
+        return F.linear(x, weight, bias)
+    
+    def reset_parameters(self):
+        mu_range = 1 / math.sqrt(self.in_features)
+        
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.weight_sigma.data.fill_(self.std_init * mu_range)
+        
+        self.bias_mu.data.uniform_(-mu_range, mu_range)
+        self.bias_sigma.data.fill_(self.std_init * mu_range)
+    
+    def reset_noise(self):
+        epsilon_in  = self._scale_noise(self.in_features)
+        epsilon_out = self._scale_noise(self.out_features)
+        
+        self.weight_epsilon.copy_(epsilon_out.outer(epsilon_in))
+        self.bias_epsilon.copy_(epsilon_out)
+    
+    def _scale_noise(self, size):
+        epsilon = torch.randn(size)
+        epsilon = epsilon.sign().mul(epsilon.abs().sqrt())
+        return epsilon
 
 
 class CartpoleNetwork(nn.Module):
